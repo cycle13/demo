f6927bf907e4bc69fb4a394b55b2936702d6a114
wangyanjun <pumpsim@163.com>
2020-09-25 16:15:25 +0800

    编写获取周口其它区县数据爬虫

--- 周口市各区县数据获取.py
+++ 周口市各区县数据获取.py
@@ -1,10 +1,11 @@
 import requests
 import pandas as pd
+import json
+import xlwt
 import time
 
 
 session = requests.Session()
-# first_url = 'https://hepp.zc12369.com/api/pollute_map/air_quality_city?time=2020-01-21+11%3A00'
 first_url = 'https://hepp.zc12369.com/api/pollute_map/air_quality_city?time='
 headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -19,16 +20,32 @@ headers = {
         'Sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
-
 }
 
-time_range = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start='2019-9-11', end='2019-9-12'))]
-# response = session.get(first_url).text
-# print(response)
+time_range = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start='2019-9-24', end='2019-9-24'))]
+book = xlwt.Workbook()
+sheet = book.add_sheet('周口市各区县数据')
+n = 1
+sheet.write(0,0,'ID')
+sheet.write(0,1,'时间')
+sheet.write(0,2,'PM10')
+sheet.write(0,3,'PM2.5')
 
 for i in time_range:
-    for j in range(0,24):
+    for j in range(0,15):
         time.sleep(5)
         url = first_url+str(i)+'+'+str(j)+ '%3A00'
         response = session.get(url,headers = headers).text
-        print(response)
+        data = json.loads(response)
+        res = data['data']
+        for k in res:
+            if k['id'] in ['411626','411628','411627','411681','411624','411622','411621','411625','411602','411623',]:
+                sheet.write(n, 0, k['id'])
+                sheet.write(n, 1, k['updateTime'])
+                sheet.write(n, 2, k['pm10'])
+                sheet.write(n, 3, k['pm25'])
+                n+=1
+                print(k)
+        # print(res)
+
+book.save('data.xls')
