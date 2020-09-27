474a5a5f525fde0f797867bf73b3d1d5e04d643b
wangyanjun <pumpsim@163.com>
2020-09-27 14:09:08 +0800

    新增淮阳县空气质量2020年数据

--- 爬虫/淮阳日数据爬取.py
+++ 爬虫/淮阳日数据爬取.py
@@ -19,7 +19,7 @@ def day(url,timess):
     response = session.post(url= url,data=data,headers = headers).text
     return response
 
-time_range = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start='2020-9-01', end='2020-9-26'))]
+time_range = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start='2020-01-01', end='2020-9-26'))]
 book = xlwt.Workbook()
 sheet = book.add_sheet('淮阳县空气质量数据')
 n = 1
@@ -35,7 +35,7 @@ sheet.write(0,8,'AQI')
 sheet.write(0,9,'首要污染物')
 
 for i in time_range:
-    time.sleep(2)
+    time.sleep(5)
     l = day(first_url_day,str(i))
     data = json.loads(l)['data']
     for k in data:
@@ -51,6 +51,6 @@ for i in time_range:
             sheet.write(n, 8, k['aqi'])
             sheet.write(n, 9, k['primary'])
             n+=1
-            print(k)
+            print(str(i),k)
 
 book.save('data.xls')
