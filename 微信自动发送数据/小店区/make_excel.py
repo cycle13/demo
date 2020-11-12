bca5dc23fb452324ba62761eae7115dfca5d34f9
wangyanjun <pumpsim@163.com>
2020-11-12 13:20:25 +0800

    更新

---
+++ 微信自动发送数据/小店区/make_excel.py
@@ -0,0 +1,347 @@
+import pandas as pd
+import openpyxl.styles
+from openpyxl.styles import Border,Side
+from openpyxl.styles import PatternFill
+import openpyxl
+import openpyxl.styles
+from openpyxl.styles import Alignment
+from openpyxl.styles import Font,colors
+import xlrd
+import xlwt
+from pandas import read_csv
+import glob
+
+
+
+
+# excel根据字段排名
+def excel_rank(excel_file_dir,excel_filenew_dir,rank_name):
+    df = pd.read_excel(excel_file_dir)
+    # df[excel_add_name] = df[rank_name].rank(method='min',ascending=True)
+    df = df.sort_values(by=rank_name)
+    df.reset_index(drop=True, inplace=True)
+    df.to_excel(excel_filenew_dir,index=False)
+
+# excel根据字段排名
+def excel_rank_rb(excel_file_dir,excel_filenew_dir,rank_name):
+    df = pd.read_excel(excel_file_dir)
+    df = df.sort_values(by=rank_name,ascending=True)
+    # df[excel_add_name] = df[rank_name].rank(method='min', ascending=False)
+    df.reset_index(drop=True, inplace=True)
+    df.to_excel(excel_filenew_dir,index=False)
+
+def excel_rank_str(excel_file_dir,excel_filenew_dir,rank_name):
+    df = pd.read_excel(excel_file_dir)
+    df = df.sort_values(by=rank_name)
+    df.reset_index(drop=True, inplace=True)
+    df.to_excel(excel_filenew_dir,index=False)
+
+# 充填行，按照AQI
+def excel_c(excel_filenew_dir,name_c,excel_filerank_dir):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    n = 0
+    for i in range(1,9):
+        if sheet.cell(i, 2).value == name_c:
+            n = i
+    if 50 >= sheet.cell(n, 8).value >= 0:
+        color = '00E400'
+    elif 100 >= sheet.cell(n, 8).value > 50:
+        color = 'FFFF00'
+    elif 150 >= sheet.cell(n, 8).value > 100:
+        color = 'FF7E00'
+    elif 200 >= sheet.cell(n, 8).value > 150:
+        color = 'FF0000'
+    elif 300 >= sheet.cell(n, 8).value > 200:
+        color = '99004C'
+    else:
+        color = '7E0023'
+    fille=PatternFill("solid",fgColor=color)
+    for j in range(1,10):
+        sheet.cell(n, j).fill = fille
+    wb.save(excel_filerank_dir)
+
+
+def excel_bz_six(excel_filenew_dir,name_c,excel_filerank_dir):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    n = 0
+    for i in range(1,8):
+        if sheet.cell(i, 2).value == name_c:
+            n = i
+    if 50 >= sheet.cell(n, 11).value >= 0:
+        color = '00E400'
+    elif 100 >= sheet.cell(n, 11).value > 50:
+        color = 'FFFF00'
+    elif 150 >= sheet.cell(n, 11).value > 100:
+        color = 'FF7E00'
+    elif 200 >= sheet.cell(n, 11).value > 150:
+        color = 'FF0000'
+    elif 300 >= sheet.cell(n, 11).value > 200:
+        color = '99004C'
+    else:
+        color = '7E0023'
+    fille=PatternFill("solid",fgColor=color)
+    for j in range(1,13):
+        sheet.cell(n, j).fill = fille
+    wb.save(excel_filerank_dir)
+
+
+def excel_bz_any(excel_filenew_dir,name_c,excel_filerank_dir):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    n = 0
+    for i in range(1,13):
+        if sheet.cell(i, 2).value == name_c:
+            n = i
+    if 50 >= sheet.cell(n, 9).value >= 0:
+        color = '00E400'
+    elif 100 >= sheet.cell(n, 9).value > 50:
+        color = 'FFFF00'
+    elif 150 >= sheet.cell(n, 9).value > 100:
+        color = 'FF7E00'
+    elif 200 >= sheet.cell(n, 9).value > 150:
+        color = 'FF0000'
+    elif 300 >= sheet.cell(n, 9).value > 200:
+        color = '99004C'
+    else:
+        color = '7E0023'
+    fille=PatternFill("solid",fgColor=color)
+    for j in range(1,11):
+        sheet.cell(n, j).fill = fille
+    wb.save(excel_filerank_dir)
+
+
+def excel_add(excel_filenew_dir,name_c,excel_filerank_dir):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    n = 0
+    for i in range(1,13):
+        if sheet.cell(i, 2).value == name_c:
+            n = i
+    if 50 >= sheet.cell(n, 9).value >= 0:
+        color = '00E400'
+    elif 100 >= sheet.cell(n, 9).value > 50:
+        color = 'FFFF00'
+    elif 150 >= sheet.cell(n, 9).value > 100:
+        color = 'FF7E00'
+    elif 200 >= sheet.cell(n, 9).value > 150:
+        color = 'FF0000'
+    elif 300 >= sheet.cell(n, 9).value > 200:
+        color = '99004C'
+    else:
+        color = '7E0023'
+    fille=PatternFill("solid",fgColor=color)
+    for j in range(1,13):
+        sheet.cell(n, j).fill = fille
+    wb.save(excel_filerank_dir)
+
+def col_num(sheet,name_c):
+    n = 0
+    for i in range(1, 13):
+        if sheet.cell(i, 1).value == name_c:
+            n = i
+    return n
+
+
+def aqi_color(color_num):
+    if 50 >= color_num >= 0:
+        color = '00E400'
+    elif 100 >= color_num > 50:
+        color = 'FFFF00'
+    elif 150 >= color_num > 100:
+        color = 'FF7E00'
+    elif 200 >= color_num > 150:
+        color = 'FF0000'
+    elif 300 >= color_num > 200:
+        color = '99004C'
+    else:
+        color = '7E0023'
+    return color
+
+
+def excel_c_hour(excel_filenew_dir1,name_c1,name_c2,excel_filerank_dir1):
+    wb = openpyxl.load_workbook(excel_filenew_dir1)
+    sheet = wb["Sheet1"]
+    n = col_num(sheet,name_c1)
+    for j in range(1,5):
+        sheet.cell(n, j).font = Font(bold=True, color="FF0000")
+
+    m = col_num(sheet, name_c2)
+    for j in range(1, 5):
+        sheet.cell(m, j).font = Font(bold=True, color="FF0000")
+
+    for j in range(2,11):
+        c = sheet.cell(j, 2).value
+        fille = PatternFill("solid", fgColor=aqi_color(c))
+        sheet.cell(j, 2).fill = fille
+    wb.save(excel_filerank_dir1)
+
+
+#居中对齐，通过遍历方式实现
+def set_from_center(excel_rank_insert):
+    fileName = excel_rank_insert
+    data = openpyxl.load_workbook(fileName)
+    table = data.get_sheet_by_name("Sheet1")
+    nrows = table.max_row # 获得行数
+    ncols = table.max_column
+    for i in range(nrows):
+        for j in range(ncols):
+            table.cell(row=i+1, column=j+1).alignment = Alignment(horizontal='center', vertical='center',wrap_text=True)
+    data.save(fileName)
+
+def table_border(excel_rank_insert):
+    wb = openpyxl.load_workbook(excel_rank_insert)
+    # sheet = wb["Sheet1"]
+    ws = wb.get_sheet_by_name("Sheet1")
+    nrows = ws.max_row
+    ncols = ws.max_column
+    for i in range(nrows):
+        for j in range(ncols):
+            border = Border(left=Side(border_style='thin', color ='000000'),
+            right = Side(border_style='thin', color ='000000'),
+            top = Side(border_style='thin', color ='000000'),
+            bottom = Side(border_style='thin', color ='000000'))
+            ws.cell(i+1, j+1).border = border
+    wb.save(excel_rank_insert)
+
+
+def table_font(excel_filerank_dir,name_table,excel_rank_insert):
+    wb = openpyxl.load_workbook(excel_filerank_dir)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    sheet.column_dimensions['B'].width = 10
+    sheet.column_dimensions['I'].width = 12
+    # sheet.column_dimensions['D'].width = 15
+    sheet.row_dimensions[1].height = 20
+    sheet.cell(1, 1).value = name_table
+    sheet["A1"].font = Font(size = 16,bold = True,color = "FF0000")
+    sheet.merge_cells('A1:I1')
+    wb.save(excel_rank_insert)
+    set_from_center(excel_rank_insert)
+    table_border(excel_rank_insert)
+
+
+def table_font_rb(excel_filenew_dir,name_table,excel_rank_insert):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    sheet.column_dimensions['A'].width = 20
+    sheet.column_dimensions['B'].width = 20
+    sheet.column_dimensions['C'].width = 20
+    sheet.row_dimensions[1].height = 20
+    sheet.cell(1, 1).value = name_table
+    sheet["A1"].font = Font(size = 14,bold = True,color = "FF0000")
+    sheet.merge_cells('A1:C1')
+    wb.save(excel_rank_insert)
+    set_from_center(excel_rank_insert)
+    table_border(excel_rank_insert)
+
+
+def table_font_db(excel_filenew_dir,name_table,excel_rank_insert):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    # sheet.column_dimensions['A'].width = 20
+    sheet.column_dimensions['B'].width = 20
+    # sheet.column_dimensions['E'].width = 20
+    sheet.row_dimensions[1].height = 30
+    sheet.cell(1, 1).value = name_table
+    sheet["A1"].font = Font(size = 10,bold = True,color = "FF0000")
+    sheet.merge_cells('A1:C1')
+    wb.save(excel_rank_insert)
+    set_from_center(excel_rank_insert)
+    table_border(excel_rank_insert)
+
+
+def table_font_str(excel_filenew_dir,name_table,excel_rank_insert,excel_lable):
+    wb = openpyxl.load_workbook(excel_filenew_dir)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    sheet.column_dimensions['A'].width = 18
+    sheet.column_dimensions['B'].width = 10
+    sheet.column_dimensions['C'].width = 20
+    sheet.column_dimensions['D'].width = 20
+    sheet.column_dimensions['E'].width = 18
+    sheet.row_dimensions[1].height = 30
+    sheet.cell(1, 1).value = name_table
+    sheet["A1"].font = Font(size = 14,bold = True,color = "FF0000")
+    sheet.merge_cells('A1:E1')
+    sheet.cell(18, 1).value = excel_lable
+    sheet["A18"].font = Font(size=10, bold=True, color="FF0000")
+    sheet.merge_cells('A18:E18')
+    sheet['A18'].alignment = Alignment(horizontal='left', vertical='center')
+    wb.save(excel_rank_insert)
+    set_from_center(excel_rank_insert)
+    table_border(excel_rank_insert)
+
+
+def table_font_six(excel_filerank_dir1, name_table1, excel_rank_insert1):
+    wb = openpyxl.load_workbook(excel_filerank_dir1)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    # sheet.column_dimensions['B'].width = 20
+    sheet.column_dimensions['C'].width = 10
+    sheet.column_dimensions['L'].width = 15
+    sheet.row_dimensions[1].height = 30
+    sheet.cell(1, 1).value = name_table1
+    sheet["A1"].font = Font(size = 14,bold = True,color = "FF0000")
+    sheet.merge_cells('A1:L1')
+    wb.save(excel_rank_insert1)
+    set_from_center(excel_rank_insert1)
+    table_border(excel_rank_insert1)
+
+
+def table_font_any(excel_filerank_dir2, name_table2, excel_rank_insert2):
+    wb = openpyxl.load_workbook(excel_filerank_dir2)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    # sheet.column_dimensions['B'].width = 20
+    # sheet.column_dimensions['C'].width = 20
+    sheet.column_dimensions['J'].width = 15
+    sheet.row_dimensions[1].height = 30
+    sheet.cell(1, 1).value = name_table2
+    sheet["A1"].font = Font(size = 14,bold = True,color = "FF0000")
+    sheet.merge_cells('A1:J1')
+    wb.save(excel_rank_insert2)
+    set_from_center(excel_rank_insert2)
+    table_border(excel_rank_insert2)
+
+
+def table_font_add(excel_filerank_dir2, name_table2, excel_rank_insert2):
+    wb = openpyxl.load_workbook(excel_filerank_dir2)
+    sheet = wb["Sheet1"]
+    # sheet.insert_rows(1)
+    sheet.column_dimensions['A'].width = 13
+    sheet.column_dimensions['B'].width = 10
+    sheet.column_dimensions['J'].width = 13
+    sheet.column_dimensions['L'].width = 10
+    # sheet.row_dimensions[1].height = 30
+    sheet.cell(13, 1).value = name_table2
+    sheet["A13"].font = Font(size = 10,bold = True,color = "FF0000")
+    sheet.merge_cells('A13:L13')
+    wb.save(excel_rank_insert2)
+    set_from_center(excel_rank_insert2)
+    table_border(excel_rank_insert2)
+
+
+def table_font_hour(excel_filerank_dir1, name_table1, excel_rank_insert1):
+    wb = openpyxl.load_workbook(excel_filerank_dir1)
+    sheet = wb["Sheet1"]
+    sheet.insert_rows(1)
+    sheet.column_dimensions['A'].width = 12
+    sheet.column_dimensions['B'].width = 12
+    sheet.column_dimensions['D'].width = 15
+    # sheet.column_dimensions['J'].width = 13
+    # sheet.column_dimensions['L'].width = 10
+    # sheet.row_dimensions[1].height = 30
+    sheet.cell(1, 1).value = name_table1
+    sheet["A1"].font = Font(size = 12,bold = True,color = "000000")
+    sheet.merge_cells('A1:D1')
+    sheet.cell(12, 1).value = '(数据来源于网络，未经审核，仅供参考)'
+    sheet["A12"].font = Font(size=8, bold=True, color="000000")
+    sheet.merge_cells('A12:D12')
+    wb.save(excel_rank_insert1)
+    set_from_center(excel_rank_insert1)
+    table_border(excel_rank_insert1)
+
---
+++ 微信自动发送数据/小店区/xiaodiansend.py
@@ -0,0 +1,121 @@
+import make_excel
+import send_text_image
+import windows_opr
+
+
+
+def send_text(name,l):
+    windows_opr.FindWindow(name)
+    send_text_image.setText(l)
+    windows_opr.send()
+    windows_opr.CloseWindow(name)
+
+def send_excel_pic(name, excel_file_dir, excel_filenew_dir, excel_filerank_dir, excel_rank_insert, rank_name, name_c,name_table):
+    windows_opr.FindWindow(name)
+    make_excel.excel_rank(excel_file_dir,excel_filenew_dir,rank_name)
+    make_excel.excel_c(excel_filenew_dir,name_c,excel_filerank_dir)
+    make_excel.table_font(excel_filerank_dir,name_table,excel_rank_insert)
+    send_text_image.excel_catch_screen(excel_rank_insert)
+    windows_opr.send()
+    windows_opr.CloseWindow(name)
+
+def send_excel_pic_doble(name, excel_file_dir1, excel_file_dir2,excel_filenew_dir1,excel_filenew_dir2,excel_rank_insert1,excel_rank_insert2,rank_name1,rank_name2,name_table1,name_table2):
+    windows_opr.FindWindow(name)
+    make_excel.excel_rank_rb(excel_file_dir1,excel_filenew_dir1,rank_name1)
+    make_excel.table_font_db(excel_filenew_dir1, name_table1, excel_rank_insert1)
+    send_text_image.excel_catch_screen_db(excel_rank_insert1)
+    windows_opr.send()
+
+    make_excel.excel_rank_rb(excel_file_dir2, excel_filenew_dir2, rank_name2)
+    make_excel.table_font_db(excel_filenew_dir2,name_table2,excel_rank_insert2)
+    send_text_image.excel_catch_screen_db(excel_rank_insert2)
+    windows_opr.send()
+    windows_opr.CloseWindow(name)
+
+
+def daily():
+    # 发送文本
+    name = '王彦军'
+    l = "【空气质量播报】\n{}，小店区综合指数为{}，城六区排名{}：\n" \
+        "太原六区综合指数由小到大排名如下：\n" \
+        "{}综合指数为{}，排名{}；\n" \
+        "{}综合指数为{}，排名{}；\n" \
+        "{}综合指数为{}，排名{}；\n" \
+        "{}综合指数为{}，排名{}；\n" \
+        "{}综合指数为{}，排名{}；\n" \
+        "{}综合指数为{}，排名{}。".format('2020年11月3日','4.94','第六','万柏林区','3.22','第一',
+                                    '尖草坪区','3.83','第二','迎泽区','4.27','第三','晋源区','4.35','第四',
+                                    '杏花岭区','4.56','第五','小店区','4.94','第六')
+    send_text(name, l)
+
+    # 发送excel截图
+    excel_file_dir = r'excelfiles\小店区日报数据.xlsx'
+    excel_filenew_dir = r'excelfiles\小店区日报数据日报排名.xlsx'
+    excel_filerank_dir = r'excelfiles\小店区日报数据排名充填.xlsx'
+    excel_rank_insert = r'D:\Program Files\pycharm\微信自动发送数据\小店区\excelfiles\小店区日报数据排名插入.xlsx'
+    rank_name = '排名'
+    name_c = '小店区'
+    name_table = '空气质量播报'
+    send_excel_pic(name, excel_file_dir, excel_filenew_dir, excel_filerank_dir, excel_rank_insert, rank_name, name_c,name_table)
+
+
+    l = "{}小店区街道乡镇PM2.5、PM10浓度均值排名如下表所示：".format('2020年11月3日')
+    send_text(name, l)
+
+
+    # 发送excel截图
+    excel_file_dir1 = r'excelfiles\小店区街道乡镇PM2.5数据.xlsx'
+    excel_file_dir2 = r'excelfiles\小店区街道乡镇PM10数据.xlsx'
+    excel_filenew_dir1 = r'excelfiles\小店区街道乡镇数据PM2.5排名.xlsx'
+    excel_filenew_dir2 = r'excelfiles\小店区街道乡镇数据PM10排名.xlsx'
+    # excel_filerank_dir1 = r'excelfiles\小店区街道乡镇数据PM2.5排名充填.xlsx'
+    # excel_filerank_dir2 = r'excelfiles\小店区街道乡镇数据PM10排名充填.xlsx'
+    excel_rank_insert1 = r'D:\Program Files\pycharm\微信自动发送数据\小店区\excelfiles\小店区街道乡镇数据PM2.5排名插入.xlsx'
+    excel_rank_insert2 = r'D:\Program Files\pycharm\微信自动发送数据\小店区\excelfiles\小店区街道乡镇数据PM10排名插入.xlsx'
+    rank_name1 = 'PM2.5'
+    rank_name2 = 'PM10'
+    name_table1= '2020年11月03日小店区街道乡镇审核前PM2.5浓度均值排名'
+    name_table2 = '2020年11月03日小店区街道乡镇审核前PM10浓度均值排名'
+
+    send_excel_pic_doble(name, excel_file_dir1, excel_file_dir2,excel_filenew_dir1,excel_filenew_dir2,excel_rank_insert1,excel_rank_insert2,rank_name1,rank_name2,name_table1,name_table2)
+
+
+
+
+def send_excel_pic_hour(name, excel_file_dir1, excel_file_dir2,excel_filenew_dir1,excel_filenew_dir2,excel_filerank_dir1,excel_filerank_dir2,excel_rank_insert1,excel_rank_insert2,rank_name1,rank_name2,name_table1,name_table2,name_c1,name_c2):
+    windows_opr.FindWindow(name)
+    make_excel.excel_rank_rb(excel_file_dir1,excel_filenew_dir1,rank_name1)
+    make_excel.excel_c_hour(excel_filenew_dir1,name_c1,name_c2,excel_filerank_dir1)
+    make_excel.table_font_hour(excel_filerank_dir1, name_table1, excel_rank_insert1)
+    send_text_image.excel_catch_screen_hour(excel_rank_insert1)
+    windows_opr.send()
+
+    make_excel.excel_rank_rb(excel_file_dir2, excel_filenew_dir2, rank_name2)
+    make_excel.excel_c_hour(excel_filenew_dir2, name_c1, name_c2, excel_filerank_dir2)
+    make_excel.table_font_hour(excel_filerank_dir2, name_table2, excel_rank_insert2)
+    send_text_image.excel_catch_screen_hour(excel_rank_insert2)
+    windows_opr.send()
+    windows_opr.CloseWindow(name)
+
+
+
+def hoursend():
+    name = '王彦军'
+    # 发送excel截图
+    excel_file_dir1 = r'excelfiles\小店区两站点AQI数据.xlsx'
+    excel_file_dir2 = r'excelfiles\小店区两站点综合指数数据.xlsx'
+    excel_filenew_dir1 = r'excelfiles\小店区两站点AQI数据排名.xlsx'
+    excel_filenew_dir2 = r'excelfiles\小店区两站点综合指数数据排名.xlsx'
+    excel_filerank_dir1 = r'excelfiles\小店区两站点AQI数据排名充填.xlsx'
+    excel_filerank_dir2 = r'excelfiles\小店区两站点综合指数数据排名充填.xlsx'
+    excel_rank_insert1 = r'D:\Program Files\pycharm\微信自动发送数据\小店区\excelfiles\小店区两站点AQI数据排名插入.xlsx'
+    excel_rank_insert2 = r'D:\Program Files\pycharm\微信自动发送数据\小店区\excelfiles\小店区两站点综合指数数据排名插入.xlsx'
+    rank_name1 = 'AQI'
+    rank_name2 = '综合指数'
+    name_c1 = '坞城'
+    name_c2 = '小店'
+    name_table1 = '2020年11月4日小店区2站点AQI排名'
+    name_table2 = '2020年11月4日小店区2站点综合指数排名'
+
+    send_excel_pic_hour(name, excel_file_dir1, excel_file_dir2,excel_filenew_dir1,excel_filenew_dir2,excel_filerank_dir1,excel_filerank_dir2,excel_rank_insert1,excel_rank_insert2,rank_name1,rank_name2,name_table1,name_table2,name_c1,name_c2)
+
