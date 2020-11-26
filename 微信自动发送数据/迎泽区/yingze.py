9b50530a40e3fa2bcf85767eb35c2303ce15f8f5
wangyanjun <pumpsim@163.com>
2020-11-26 11:30:53 +0800

    更新迎泽发送代码

--- 微信自动发送数据/迎泽区/yingze.py
+++ 微信自动发送数据/迎泽区/yingze.py
@@ -8,11 +8,14 @@ if __name__ == '__main__':
     # time_list1 = ['10:30','16:30','20:30']
     name = input('请输入要发送的微信名称：')
     while True:
-        now_time = datetime.datetime.strftime(datetime.datetime.now(),'%H:%M')
-        if now_time in time_list:
-            print(now_time)
-            yingzesend.hoursend(name)
-        # elif now_time in time_list1:
-        #     print(now_time)
-        #     yingzesend.hourjc(name)
-        time.sleep(60)
+        try:
+            now_time = datetime.datetime.strftime(datetime.datetime.now(),'%H:%M')
+            if now_time in time_list:
+                print(now_time)
+                yingzesend.hoursend(name)
+            # elif now_time in time_list1:
+            #     print(now_time)
+            #     yingzesend.hourjc(name)
+            time.sleep(60)
+        except:
+            pass
