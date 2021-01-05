97c3d510c5d9fd8ce9177d05bafbd09c80fdc365
王彦军 <pumpsim@163.com>
2021-01-05 16:25:23 +0800

    年报可视化

--- 爬虫/周口市各区县数据获取.py
+++ 爬虫/周口市各区县数据获取.py
@@ -22,7 +22,7 @@ headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
 }
 
-time_range = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start='2020-11-05', end='2020-11-11'))]
+time_range = [x.strftime('%Y-%m-%d') for x in list(pd.date_range(start='2020-12-26', end='2020-12-28'))]
 book = xlwt.Workbook()
 sheet = book.add_sheet('周口市各区县数据')
 n = 1
@@ -30,22 +30,42 @@ sheet.write(0,0,'ID')
 sheet.write(0,1,'时间')
 sheet.write(0,2,'PM10')
 sheet.write(0,3,'PM2.5')
+sheet.write(0,4,'SO2')
+sheet.write(0,5,'NO2')
+sheet.write(0,6,'CO')
+sheet.write(0,7,'O3')
+sheet.write(0,8,'AQI')
+sheet.write(0,9,'首要污染物')
+sheet.write(0,10,'综合指数')
+sheet.write(0,11,'空气质量等级')
 
 for i in time_range:
     for j in range(0,24):
-        time.sleep(5)
-        url = first_url+str(i)+'+'+str(j)+ '%3A00'
-        response = session.get(url,headers = headers).text
-        data = json.loads(response)
-        res = data['data']
-        for k in res:
-            if k['id'] in ['411626','411628','411627','411681','411624','411622','411621','411625','411602','411623',]:
-                sheet.write(n, 0, k['areaAllName'])
-                sheet.write(n, 1, k['updateTime'])
-                sheet.write(n, 2, k['pm10'])
-                sheet.write(n, 3, k['pm25'])
-                n+=1
-                print(k)
-        # print(res)
+        try:
+            time.sleep(5)
+            url = first_url+str(i)+'+'+str(j)+ '%3A00'
+            response = session.get(url,headers = headers).text
+            data = json.loads(response)
+            res = data['data']
+            for k in res:
+                if k['id'] in ['411626','411628','411627','411681','411624','411622','411621','411625','411602','411623',]:
+                    sheet.write(n, 0, k['areaAllName'])
+                    sheet.write(n, 1, k['updateTime'])
+                    sheet.write(n, 2, k['pm10'])
+                    sheet.write(n, 3, k['pm25'])
+                    sheet.write(n, 4, k['so2'])
+                    sheet.write(n, 5, k['no2'])
+                    sheet.write(n, 6, k['co'])
+                    sheet.write(n, 7, k['o3'])
+                    sheet.write(n, 8, k['aqi'])
+                    sheet.write(n, 9, k['mainPol'])
+                    sheet.write(n, 10, k['zonghezhishu'])
+                    sheet.write(n, 11, k['dataLevel'])
+                    n+=1
+                    print(k)
+            # print(res)
+        except:
+            print('数据异常')
+            time.sleep(30)
 
-book.save('hourdata11月11日.xls')
+book.save('2020年12月小时数据.xls')
