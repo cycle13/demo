b661b12f3e5cf1fe90d188cebf978b6691c98af0
王彦军 <pumpsim@163.com>
2021-01-06 13:32:25 +0800

    更新淮阳新推送代码

--- 微信自动发送数据/淮阳县demo/demo.py
+++ 微信自动发送数据/淮阳县demo/demo.py
@@ -126,13 +126,13 @@ def sendd_text(name):
     if ((int(huai[0][0])-int(huai[1][0]))/int(huai[1][0]))>=0.3:
         l+='淮阳县PM2.5上升较快'
     if ((int(huai[0][1]) - int(huai[1][1])) / int(huai[1][1])) >= 0.3:
-        l += '淮阳县PM10上升较快'
+        l += '，淮阳县PM10上升较快'
     if (int(huai[0][0])-int(tai[0][0])) >= 10:
-        l+='淮阳县跟太康县PM2.5差距较大'
+        l+='，淮阳县跟太康县PM2.5差距较大'
     if (int(huai[0][1])-int(tai[0][1])) >= 10:
-        l+='淮阳县跟太康县PM10差距较大'
+        l+='，淮阳县跟太康县PM10差距较大'
     if len(l) > 0:
-        l = '@所有人 '+l
+        l = '@所有人 '+l+'。'
     send_text(name,l)
 
 
