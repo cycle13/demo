18621d9a91e69441a5128e83f85148fbc1366dcb
王彦军 <pumpsim@163.com>
2021-03-25 17:29:06 +0800

    CMAQ完善

--- 常规学习/CMAQ可视化/cmaq.py
+++ 常规学习/CMAQ可视化/cmaq.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import cartopy.io.shapereader as shpreader
 import cartopy.crs as ccrs
 import pandas as pd
+from cartopy.mpl.gridliner import LATITUDE_FORMATTER, LONGITUDE_FORMATTER
+import demo
 import maskout
 import imageio
 # from moviepy.editor import ImageSequenceClip
@@ -12,7 +14,7 @@ plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
 
 
-data = Dataset(r'data/CCTM_APMDIAG_v531_gcc9.1.0_Bench_2020_SX_20210131.nc', mode='r')
+data = Dataset(r'data/CCTM_APMDIAG_v531_gcc9.1.0_Bench_2020_SX_20210321.nc', mode='r')
 data1 = pd.read_csv('data/Export_Output_7.csv',encoding='utf-8')
 ROW = data.dimensions['ROW'].size
 COL = data.dimensions['COL'].size
@@ -69,26 +71,53 @@ for i in range(ROW):
 
 
 # lon, lat = np.meshgrid(lons, lats)
-#
-PM25AC = data.variables['PM25AC'][2][0]
-# print(data.variables['PM25AC'])
-# print(len(lon),len(lat),np.shape(PM25AC))
-pb = PM25AC
-fig = plt.figure(figsize=(10,5))
+for i in range(71):
+    PM25 = data.variables['PM25AC'][i][0]+data.variables['PM25AT'][i][0]+data.variables['PM25CO'][i][0]
+    pb = PM25
+    maxx = np.max(PM25)
+    minx = np.min(PM25)
+    proj = ccrs.LambertConformal(central_latitude=37.75, central_longitude=112.35)
+    fig, ax = plt.subplots(figsize=(5, 10), subplot_kw=dict(projection=proj))  # 建立页面
+    ax.set_extent([110, 114.5, 34.5, 41], ccrs.PlateCarree())  # 设置经纬度范围
+    cf = plt.contourf(lon,lat, pb,transform=ccrs.PlateCarree(),cmap="jet",vmin=minx, vmax=maxx)
+    plt.scatter(112, 37, marker='o', s=6, color="k")
+    fig.canvas.draw()
+    xticks = list(range(-180, 180, 1))
+    yticks = list(range(-90, 90, 1))
+    ax.gridlines(xlocs=xticks, ylocs=yticks, linewidth=0.2, linestyle='--')
+    ax.xaxis.set_major_formatter(LONGITUDE_FORMATTER)
+    ax.yaxis.set_major_formatter(LATITUDE_FORMATTER)
+    demo.lambert_xticks(ax, xticks)
+    demo.lambert_yticks(ax, yticks)
+    cb=fig.colorbar(cf,shrink=0.8)
+    cb.set_label("单位："+r"$\rm \mu g \cdot m^{-3}$",fontsize=10)
+    cb.ax.tick_params(direction='out',length=5)
+    plt.title('PM2.5浓度',fontsize=12)
+    # clip = maskout.shp2clip(cf,ax,r'shanxi/shanxi.shp',1)
+    shpname = r'shanxi3/Export_Output_5.shp'
+    # shpname = r'sx_shp/county_pophu.shp'
+    adm1_shapes=list(shpreader.Reader(shpname).geometries())
+    ax.add_geometries(adm1_shapes[:],ccrs.PlateCarree(),edgecolor='k',facecolor='')
+    # plt.show()
+    plt.savefig('pic/{0}.png'.format(str(i)))
 
 
-ax = plt.axes(projection=ccrs.PlateCarree())
-cf = plt.contourf(lon,lat, pb,transform=ccrs.PlateCarree(),cmap="jet")
-ax.set_xticks(np.arange(110,115,1),crs = ccrs.PlateCarree())    # x轴
-ax.set_yticks(np.arange(34,41,1),crs = ccrs.PlateCarree())
-cb=fig.colorbar(cf,shrink=0.8)
-cb.set_label("单位："+r"$\rm \mu g \cdot m^{-3}$",fontsize=10)
-cb.ax.tick_params(direction='out',length=5)
-plt.title('PM2.5浓度',fontsize=12)
-# clip = maskout.shp2clip(cf,ax,r'shanxi/shanxi.shp',1)
-shpname = r'shanxi3/Export_Output_5.shp'
-# shpname = r'sx_shp/county_pophu.shp'
-adm1_shapes=list(shpreader.Reader(shpname).geometries())
-ax.add_geometries(adm1_shapes[:],ccrs.PlateCarree(),edgecolor='k',facecolor='')
-# plt.show()
-plt.savefig('pic/1.png')
+
+def create_gif(image_list, gif_name, duration = 1.0):
+    '''
+    :param image_list: 这个列表用于存放生成动图的图片
+    :param gif_name: 字符串，所生成gif文件名，带.gif后缀
+    :param duration: 图像间隔时间
+    :return:
+    '''
+    frames = []
+    for image_name in image_list:
+        frames.append(imageio.imread(image_name))
+
+    imageio.mimsave(gif_name, frames, 'GIF', duration=duration)
+    return
+
+
+img_names = ['pic/'+str(i)+'.png' for i in range(0,71)]
+print(img_names)
+create_gif(img_names,'pic/山西省PM25.gif', duration=0.5)
