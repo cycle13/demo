8eb42acf7742453de4f3a68b114a69ef886baba8
王彦军 <pumpsim@163.com>
2021-05-15 13:35:35 +0800

    更新EKMA曲线

--- 常规学习/EKMA曲线成品/data_p.py
+++ 常规学习/EKMA曲线成品/data_p.py
@@ -5,10 +5,32 @@ import json
 
 session = requests.Session()
 headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Content-Length': '2484',
+        'Content-Type': 'application/x-www-form-urlencoded',
         'Host': 'www.shodor.org',
         'Origin': 'http://www.shodor.org',
         'Proxy-Connection': 'keep-alive',
         'Referer': 'http://www.shodor.org/ekma//model/ozipform.html',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
+
+}
+headers1 = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Host': 'www.shodor.org',
+        'Proxy-Connection': 'keep-alive',
+        'Referer': 'http://www.shodor.org/cgi-bin/ozip/ozip.pl',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
+
+}
+headers2 = {
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
 
@@ -21,24 +43,24 @@ url = 'http://www.shodor.org/cgi-bin/ozip/ozip.pl'
 def data_p(name):
     with open('data_data/data2.json', encoding='utf-8') as f:
         data = json.load(f)
-    session.get(fir_url,headers = headers)
-    time.sleep(4)
+    session.get(fir_url,headers = headers2)
+    time.sleep(10)
     print('正在计算！')
     res1 = session.post(url,data = data,headers = headers)
     html = etree.HTML(res1.text)
     all_url = html.xpath('/html/body/li/a/@href')
     oziso_url = 'http://www.shodor.org' + all_url[1]
-    res = session.get(oziso_url, headers=headers)
+    time.sleep(10)
+    res = session.get(oziso_url,headers = headers1)
     txt = open('data/output.txt', 'w')
     txt.write(res.text)
-    time.sleep(1)
+    time.sleep(10)
     oziso_url = 'http://www.shodor.org'+all_url[2]
-    time.sleep(4)
+    time.sleep(10)
     print('正在导出数据！')
-    res = session.get(oziso_url,headers = headers)
+    res = session.get(oziso_url,headers = headers1)
     txt = open('data/'+name+'.txt', 'w')
     txt.write(res.text)
     voc = data['basevoc']
     nox = data['basenox']
     return voc,nox
-
---
+++ 常规学习/怀俄明州探空图/demo.py
@@ -0,0 +1,38 @@
+import requests
+import time
+import os
+import pandas as pd
+
+
+
+session = requests.Session()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+urls = 'http://weather.uwyo.edu/upperair/images/{0}{1}.{2}.b{3}.png'
+urll = 'http://weather.uwyo.edu/cgi-bin/bufrraob.py?datetime={0}%20{1}:00:00&id={2}&type=PNG:{3}'
+station = ['50527', '50557', '50774', '50953', '51076', '51431', '51463', '51644', '51709', '51777','51828', '51839', '52203', '52267', '52323', '52418', '52533', '52681', '52818', '52836', '52866', '52983', '53068', '53463', '53513', '53614', '53772', '53845', '53915', '54102', '54135', '54161', '54218', '54292', '54374', '54511', '54662', '54727', '54857', '55299', '55591', '56029', '56080', '56146', '56187', '56691', '56739', '56778', '56964', '56985', '57083', '57127', '57131', '57178', '57447', '57461', '57494', '57516', '57687', '57749', '57816', '57957', '57972', '57993', '58027', '58150', '58203', '58238', '58362', '58424', '58457', '58606', '58633', '58665', '58725', '58847', '59134', '59211', '59265', '59280', '59316', '59431', '59758']
+dt = ['00','12']
+png = ['SKEWT','STUVE','STUVE10']
+begin = '2021-05-01'
+end = '2021-05-07'
+time_range = [x.strftime('%Y%m%d') for x in list(pd.date_range(start=begin, end=end))]
+
+for i in time_range:
+    for k in station:
+        for j in dt:
+            for n in png:
+                try:
+                    url = urls.format(i, j, k,n.lower())
+                    urlss = urll.format(i[0:4] + '-' + i[4:6] + '-' + i[6:8], j, k, n)
+                    res = session.get(urlss, headers=headers).text
+                    time.sleep(1)
+                    res = session.get(url, headers=headers)
+                    print(res)
+                    if not os.path.exists('data/'+i[0:4]+'/'+i[4:6]+'/'+k+'/'+n):
+                        os.makedirs('data/'+i[0:4]+'/'+i[4:6]+'/'+k+'/'+n)
+                    with open('data/'+i[0:4]+'/'+i[4:6]+'/'+k+'/'+n+'/'+ i[6:8]+'-'+j+'-'+k + '.png', 'wb') as f:
+                        f.write(res.content)
+                    time.sleep(1)
+                except:
+                    time.sleep(3)
