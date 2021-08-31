750b336a42764245c847305cb3eceb1fb7961ef6
王彦军 <pumpsim@163.com>
2021-08-31 14:15:23 +0800

    雷达图完善

--- 特征雷达图/polar_pic.py
+++ 特征雷达图/polar_pic.py
@@ -201,11 +201,103 @@ def area_hour_acc_data():
         data_visual.plot_radar_time(newfile_dir, k, name[0])
 
 
+def area_hour_acc_data1():
+    area_name = '淮阳县'
+    start_date_time = '2021-01-01 02:00:00'
+    end_date_time = '2021-01-02 04:00:00'
+    start_date_time1 = datetime.strptime(start_date_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+    end_date_time1 = datetime.strptime(end_date_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+    date_times = pd.date_range(start_date_time, end_date_time,freq='D')
+    for date_time in date_times:
+        date_time = datetime.strptime(date_time.strftime("%Y-%m-%d"),"%Y-%m-%d")
+        my_datatime = pd.date_range(date_time.strftime('%Y-%m-%d'), '2050-01-01')
+        result = my_datatime[0].strftime('%Y-%m-%d')
+        start_date = (my_datatime[0] + datatime.timedelta(days=-31)).strftime("%Y-%m-%d")
+        end_date = (my_datatime[0] + datatime.timedelta(days=-1)).strftime("%Y-%m-%d")
+        file_dir = 'excelfile/周口数据.xls'
+        file_cen_dir = 'excelfile/周口数据中间.xls'
+        newfile_dir = 'excelfile/周口数据.xlsx'
+        newfile_acc_dir = 'excelfile/周口截取数据.xlsx'
+        df1 = pd.read_excel(file_dir,sheet_name='Sheet1')
+        df1.index = df1['时间']
+        df1 = df1[start_date:end_date]
+        if datetime.strptime(end_date_time, '%Y-%m-%d %H:%M:%S')>=datetime.strptime(date_time.strftime('%Y-%m-%d')+' 23:00:00', '%Y-%m-%d %H:%M:%S'):
+            if datetime.strptime(start_date_time, '%Y-%m-%d %H:%M:%S')>=datetime.strptime(date_time.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S'):
+                my_datatime = pd.date_range(start_date_time,
+                                            date_time.strftime('%Y-%m-%d') + ' 23:00:00', freq='h')
+            else:
+                my_datatime = pd.date_range(date_time.strftime('%Y-%m-%d %H:%M:%S'), date_time.strftime('%Y-%m-%d')+' 23:00:00', freq='h')
+        else:
+            my_datatime = pd.date_range(date_time.strftime('%Y-%m-%d %H:%M:%S'), end_date_time, freq='h')
+        df2 = pd.read_excel(file_dir, sheet_name='Sheet2')
+        df2.index = df2['时间']
+        df4 = df2[my_datatime[0].strftime('%Y-%m-%d %H:%M:%S'):my_datatime[-1].strftime('%Y-%m-%d %H:%M:%S')]
+        df4.to_excel(newfile_acc_dir)
+        for i in my_datatime:
+            j = i.strftime('%Y-%m-%d %H:%M:%S')
+            k = i.strftime('%Y-%m-%d %H%M%S')
+            df3 = df2[j:j]
+            df = df1.append(df3)
+            print(j)
+            df.to_excel(file_cen_dir, index=False)
+            name = data_opt.tezheng_area(file_cen_dir, newfile_dir, i,area_name)
+            data_visual.plot_radar_time(newfile_dir, k, name[0])
 
-time_acc_data()
+
+# 滚动计算每一小时的时间累计特征雷达图
+def time_hour_acc_data1():
+    start_date_time = '2021-02-10 13:00:00'
+    end_date_time = '2021-02-13 04:00:00'
+    start_date_time1 = datetime.strptime(start_date_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+    end_date_time1 = datetime.strptime(end_date_time, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+    date_times = pd.date_range(start_date_time1, end_date_time1, freq='D')
+    print(date_times)
+    for date_time in date_times:
+        date_time = datetime.strptime(date_time.strftime("%Y-%m-%d"), "%Y-%m-%d")
+        my_datatime = pd.date_range(date_time.strftime('%Y-%m-%d'), '2050-01-01')
+        result = my_datatime[0].strftime('%Y-%m-%d')
+        start_date = (my_datatime[0] + datatime.timedelta(days=-31)).strftime("%Y-%m-%d")
+        end_date = (my_datatime[0] + datatime.timedelta(days=-1)).strftime("%Y-%m-%d")
+        file_dir = 'excelfile/合川区.xls'
+        file_cen_dir = 'excelfile/合川区中间.xls'
+        newfile_dir = 'excelfile/合川区.xlsx'
+        newfile_acc_dir = 'excelfile/合川区截取.xlsx'
+        df1 = pd.read_excel(file_dir,sheet_name='Sheet1')
+        df1.index = df1['时间']
+        df1 = pd.read_excel(file_dir, sheet_name='Sheet1')
+        df1.index = df1['时间']
+        df1 = df1[start_date:end_date]
+        if datetime.strptime(end_date_time, '%Y-%m-%d %H:%M:%S') >= datetime.strptime(
+                date_time.strftime('%Y-%m-%d') + ' 23:00:00', '%Y-%m-%d %H:%M:%S'):
+            if datetime.strptime(start_date_time, '%Y-%m-%d %H:%M:%S') >= datetime.strptime(
+                    date_time.strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S'):
+                my_datatime = pd.date_range(start_date_time,
+                                            date_time.strftime('%Y-%m-%d') + ' 23:00:00', freq='h')
+            else:
+                my_datatime = pd.date_range(date_time.strftime('%Y-%m-%d %H:%M:%S'),
+                                            date_time.strftime('%Y-%m-%d') + ' 23:00:00', freq='h')
+        else:
+            my_datatime = pd.date_range(date_time.strftime('%Y-%m-%d %H:%M:%S'), end_date_time, freq='h')
+        df2 = pd.read_excel(file_dir, sheet_name='Sheet2')
+        df2.index = df2['时间']
+        df4 = df2[my_datatime[0].strftime('%Y-%m-%d %H:%M:%S'):my_datatime[-1].strftime('%Y-%m-%d %H:%M:%S')]
+        df4.to_excel(newfile_acc_dir)
+        for i in my_datatime:
+            j = i.strftime('%Y-%m-%d %H:%M:%S')
+            k = i.strftime('%Y-%m-%d %H%M%S')
+            df3 = df2[j:j]
+            df = df1.append(df3)
+            print(j)
+            df.to_excel(file_cen_dir, index=False)
+            name = data_opt.tezheng_hour(file_cen_dir, newfile_dir, i)
+            data_visual.plot_radar_time_hour(newfile_dir, k, name[0])
+
+
+# time_acc_data()
 # area_roll_data()
 # area_data()
 # time_data()
 # time_roll_data()
 # time_hour_acc_data()
-# area_hour_acc_data()
+# area_hour_acc_data1()
+time_hour_acc_data1()
